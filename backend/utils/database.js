df0b9e60d4cb82f2c1994a40dc0e51e2d08e27dc
Maxym Ovramenko <ovramenko.maksim06@gmail.com>
2025-11-15 13:52:38 +0200

    implement full DB, cookie sessions, and live gameloop

---
+++ backend/utils/database.js
@@ -0,0 +1,31 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+//ця функція буде запускатися один раз при старті сервера
+export async function setupDatabase() {
+    //відкриваємо (або створюємо, якщо немає) файл бази даних
+    const db = await open({
+        filename: './backend/storage/pets.db',
+        driver: sqlite3.Database
+    });
+
+    //Виконуємо запит на створення таблиці, ЯКЩО вона ще не існує
+    //Це безпечно запускати кожного разу
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS Pets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ownerId TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL,
+            type TEXT NOT NULL,
+            age INTEGER DEFAULT 0,
+            health INTEGER DEFAULT 100,
+            hunger INTEGER DEFAULT 0,
+            happiness INTEGER DEFAULT 0,
+            energy INTEGER DEFAULT 0,
+            cleanliness INTEGER DEFAULT 0
+        );
+    `);
+
+    console.log("✅ База даних SQLite готова.");
+    return db;
+}
